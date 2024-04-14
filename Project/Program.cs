ffb2c5627ae16a2e56730775a61ede5568fc9f8a
PaulMaget <paul.pj.maget@gmail.com>
2024-04-14 15:44:43 +0200

    Première version publié avec Blazor

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Portfolio_Blazor;
+using Portfolio;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
